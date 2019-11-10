be12c1dcd953af82ee94aceefec36d65c0728baa
Kirill Vishnyakov <intainer7@gmail.com>
2019-11-11 02:50:55 +0300

    comments

--- inference.py
+++ inference.py
@@ -175,16 +175,16 @@ def get_ensemble_prediction(loaders, weights_path, technique="voting", threshold
         models[i].cuda()
         utils.unpack_checkpoint(checkpoint, model=models[i])
         runners[i] = SupervisedRunner(model=models[i])
-        loader_valid = {"infer": loaders["valid"]}
-        runners[i].infer(
-            model=models[i],
-            loaders=loader_valid,
-            callbacks=[
-                InferCallback()
-            ],
-            verbose=True
-        )
-        gc.collect()
+      #  loader_valid = {"infer": loaders["valid"]}
+      #  runners[i].infer(
+      #      model=models[i],
+      #      loaders=loader_valid,
+      #      callbacks=[
+      #          InferCallback()
+      #      ],
+      #      verbose=True
+      #  )
+      #  gc.collect()
     iters = 0
     pred_distr = {-1:0, 0:0, 1:0, 2:0, 3:0}
 

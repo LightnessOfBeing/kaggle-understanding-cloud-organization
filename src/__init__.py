cd0961aa41960c8cd1cf0af137cedf5a2ea9ad28
LightnessOfBeing <intainer7@gmail.com>
2020-04-16 12:17:01 +0300

    small change

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,10 @@
 from catalyst.dl import registry
 from catalyst.dl import SupervisedRunner as Runner
 from .experiment import Experiment
+
 from src.symmetric_lovasz_loss import SymmetricLovaszLoss
 from catalyst.contrib.nn import BCEDiceLoss
-from segmentation_models_pytorch import Unet
 
 registry.Criterion(BCEDiceLoss)
 registry.Criterion(SymmetricLovaszLoss)
 
-registry.Model(Unet)

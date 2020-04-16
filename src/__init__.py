c215b72cef89ed99526e3c320610376c242bfeb4
LightnessOfBeing <intainer7@gmail.com>
2020-04-16 12:29:04 +0300

    small change

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from catalyst.dl import registry
 from catalyst.dl import SupervisedRunner as Runner
+
+from .callbacks import CustomDiceCallback, CustomSegmentationInferCallback
 from .experiment import Experiment
 
 from src.symmetric_lovasz_loss import SymmetricLovaszLoss
@@ -7,4 +9,7 @@ from catalyst.contrib.nn import BCEDiceLoss
 
 registry.Criterion(BCEDiceLoss)
 registry.Criterion(SymmetricLovaszLoss)
+
+registry.Callback(CustomDiceCallback)
+registry.Callback(CustomSegmentationInferCallback)
 

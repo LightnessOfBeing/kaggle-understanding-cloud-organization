4567ef797d7f966c12f1bb1f8922fee476927e6c
LightnessOfBeing <intainer7@gmail.com>
2020-04-16 09:01:45 +0300

    import config

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from catalyst.dl import registry
 from catalyst.dl import SupervisedRunner as Runner
+from .experiment import Experiment
 from src.symmetric_lovasz_loss import SymmetricLovaszLoss
 from catalyst.contrib.nn import BCEDiceLoss
 

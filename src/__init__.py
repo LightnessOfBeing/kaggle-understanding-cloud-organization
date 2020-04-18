ceb58c3a6f109098d3b0226e219f92276164be57
LightnessOfBeing <intainer7@gmail.com>
2020-04-18 07:32:25 +0300

    changed loss

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .callbacks import CustomDiceCallback, CustomSegmentationInferCallback
 from .experiment import Experiment
 
 from src.symmetric_lovasz_loss import SymmetricLovaszLoss
-from catalyst.contrib.nn import BCEDiceLoss
+from .losses import BCEDiceLoss
 
 registry.Criterion(BCEDiceLoss)
 registry.Criterion(SymmetricLovaszLoss)
